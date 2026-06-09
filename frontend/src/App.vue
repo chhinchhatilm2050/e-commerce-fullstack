57696c830b881547fddef72fec79204224adb1d7
Chhin Chhatt <chhinchhat.ilm2050@gmail.com>
2026-06-09 09:50:08 +0700

    test rule

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@
   <p>
     Visit
     <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation test
+    read the documentation test chhat
   </p>
 </template>
 
