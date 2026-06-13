4ea90790d541fd292ba9e82de5200cadb72a8144
Chhin Chhatt <chhinchhat.ilm2050@gmail.com>
2026-06-13 13:49:19 +0700

    fix bug

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,17 +7,17 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'home',
     component: HomeView,
-    meta: { title: 'ChhatStore - Home' },
+    meta: { title: 'ChhatStore - Home', guestOnly: true },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  // scrollBehavior(to, from, savedPosition) {
-  //   if (savedPosition) return savedPosition;
-  //   return { top: 0, behavior: 'smooth' };
-  // },
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition
+    return { top: 0, behavior: 'smooth' }
+  },
 })
 
 export default router
